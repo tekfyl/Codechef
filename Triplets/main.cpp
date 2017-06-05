2a4206ef44ff6e6430ebf3e6a3e4607f30bd67a6
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-05 08:27:22 +0530

    Working

--- Triplets/main.cpp
+++ Triplets/main.cpp
@@ -4,37 +4,37 @@ using namespace std;
 
 int main(){
     std::ios_base::sync_with_stdio(false);
-        int i,t,p,q,r,a,b,c;
-
-        long long int sum=0;
-        cin >> t;
-        while(t--){
-            sum = 0;
-            cin >> p >> q >> r;
-            vector<int> A,B,C;
-            for(i=0; i<p; i++){
-                cin >> a; A.push_back(a);
-            }
-            for(i=0; i<q; i++){
-                cin >> b; B.push_back(b);
-            }
-            for(i=0; i<r; i++){
-                cin >> c; C.push_back(c);
-            }
-            for(b=0; b<q; b++){
-                int y = B[b];
-                for(a=0; a<p; a++){
-                    int x = A[a];
-                    if(x>y) continue;
-                    for(c=0; c<r; c++){
-                        if(z>y) continue;
-                        int z = C[c];
-                        sum += (x+y)*(y+z);
-                    }
+    int i,t,p,q,r,a,b,c;
+    
+    long long int sum=0;
+    cin >> t;
+    while(t--){
+        sum = 0;
+        cin >> p >> q >> r;
+        vector<int> A,B,C;
+        for(i=0; i<p; i++){
+            cin >> a; A.push_back(a);
+        }
+        for(i=0; i<q; i++){
+            cin >> b; B.push_back(b);
+        }
+        for(i=0; i<r; i++){
+            cin >> c; C.push_back(c);
+        }
+        for(b=0; b<q; b++){
+            int y = B[b];
+            for(a=0; a<p; a++){
+                int x = A[a];
+                if(x>y) continue;
+                for(c=0; c<r; c++){
+                    int z = C[c];
+                    if(z>y) continue;
+                    sum += (x+y)*(y+z);
+                    sum = sum%1000000007;
                 }
             }
-            cout << sum%1000000007 << endl;
         }
+        cout << sum << endl;
+    }
     return 0;
 }
-
