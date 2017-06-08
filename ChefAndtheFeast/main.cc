3a1628050e3677e6c30b78fc6aa279b287c3785d
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-08 18:45:59 +0530

    Working

--- ChefAndtheFeast/main.cc
+++ ChefAndtheFeast/main.cc
@@ -6,14 +6,16 @@ int main(){
     std::ios_base::sync_with_stdio(false);
         int i,n,t;
         cin >> t;
+            long long sum=0;
         while(t--){
             int n; cin >> n;
             vector<int> v;
             for(i=0; i<n; i++){
                 int in; cin >> in; v.push_back(in);
             }
-            cout << accumulate(v.begin(), v.end(), 0) << endl;
+            sum += accumulate(v.begin(), v.end(), 0);
         }
+            cout << sum << endl;
 
     return 0;
 }
